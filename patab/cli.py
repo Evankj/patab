24eb883ce4d241ba1c6c7e81a4fcea39cb15d738
Evankj <evankellyjohnston@gmail.com>
2023-05-24 15:16:54 +1000

    Formatted using Black

--- patab/cli.py
+++ patab/cli.py
@@ -5,15 +5,48 @@ from progress.bar import Bar
 from jinja2 import Template
 from patab import PatternAbstractor
 
-parser = argparse.ArgumentParser(description='Search for a pattern in files with a given extension under a directory')
-parser.add_argument('--directory', '-d', default='.', help='the directory to search in', type=str)
-parser.add_argument('--pattern', '-p', required=True, help='the pattern to search for', type=str)
-parser.add_argument('--extension', '-e', required=True, help='the file extension to search for', type=str)
-parser.add_argument('--output-file', '-o', default='matches.csv', help='the CSV file to write the results to', type=str)
-parser.add_argument('--context', '-c', default=1, help='the number of lines before and after the line on which the match was found to include', type=int)
-parser.add_argument('--template', '-t', required=True, help='Jinja2 template file to populate with found matches', type=str)
-parser.add_argument('--verbose', '-v', help='report progress to stdout', action='store_true')
+parser = argparse.ArgumentParser(
+    description="Search for a pattern in files with a given extension under a directory"
+)
+parser.add_argument(
+    "--directory", "-d", default=".", help="the directory to search in", type=str
+)
+parser.add_argument(
+    "--pattern", "-p", required=True, help="the pattern to search for", type=str
+)
+parser.add_argument(
+    "--extension",
+    "-e",
+    required=True,
+    help="the file extension to search for",
+    type=str,
+)
+parser.add_argument(
+    "--output-file",
+    "-o",
+    default="matches.csv",
+    help="the CSV file to write the results to",
+    type=str,
+)
+parser.add_argument(
+    "--context",
+    "-c",
+    default=1,
+    help="the number of lines before and after the line on which the match was found to include",
+    type=int,
+)
+parser.add_argument(
+    "--template",
+    "-t",
+    required=True,
+    help="Jinja2 template file to populate with found matches",
+    type=str,
+)
+parser.add_argument(
+    "--verbose", "-v", help="report progress to stdout", action="store_true"
+)
 args = parser.parse_args()
+
 
 def cli() -> bool:
     """
@@ -22,12 +55,17 @@ def cli() -> bool:
     Returns:
     - bool: True if the output was successfully written to the file, False otherwise.
     """
-    patab = PatternAbstractor(directory=args.directory, pattern=args.pattern, extension=args.extension, debug=args.verbose)
+    patab = PatternAbstractor(
+        directory=args.directory,
+        pattern=args.pattern,
+        extension=args.extension,
+        debug=args.verbose,
+    )
     matches = patab.get_matches()
     template: Template | None = None
     try:
         with open(args.template) as templatefile:
-                template = Template(templatefile.read())
+            template = Template(templatefile.read())
     except:
         """
         Catches errors when trying to read the template file and prints an error message.
@@ -38,17 +76,22 @@ def cli() -> bool:
         Returns:
         - bool: False, indicating that the output was not successfully written to the file.
         """
-        print(f'ERROR: Could not initialise template from template file: {args.template}')
+        print(
+            f"ERROR: Could not initialise template from template file: {args.template}"
+        )
         return False
 
-    patab.write_matches_to_template(matches=matches, template=template, output_file=args.output_file)
+    patab.write_matches_to_template(
+        matches=matches, template=template, output_file=args.output_file
+    )
     return True
+
 
 if __name__ == "__main__":
     """
     Runs the command line interface and prints a success message if the output was successfully written to the file.
     """
     if cli():
-        print(f'Template file {args.output_file} successfully generated...')
+        print(f"Template file {args.output_file} successfully generated...")
     else:
-        print('Something went wrong! Exiting...')
+        print("Something went wrong! Exiting...")
--- patab/patab.py
+++ patab/patab.py
@@ -1,4 +1,4 @@
-from os import PathLike, error
+from os import PathLike
 from typing import Dict, List
 from ripgrepy import Ripgrepy
 from progress.bar import Bar
@@ -11,12 +11,8 @@ class PatternAbstractor:
     debug = False
 
     def __init__(
-        self,
-        directory: PathLike,
-        pattern: str,
-        extension: str,
-        debug: bool = False
-        ) -> None:
+        self, directory: PathLike, pattern: str, extension: str, debug: bool = False
+    ) -> None:
         """
         Initializes an instance of the PatternAbstractor class.
 
@@ -26,9 +22,9 @@ class PatternAbstractor:
         - extension (str): The file extension to search for.
         - debug (bool, optional): Whether or not to enable debug mode. Defaults to False.
         """
-        self.directory=directory
-        self.pattern=pattern
-        self.extension=extension
+        self.directory = directory
+        self.pattern = pattern
+        self.extension = extension
         self.debug = debug
 
     def get_matches(self) -> List[Dict]:
@@ -38,11 +34,21 @@ class PatternAbstractor:
         Returns:
         - List[Dict]: A list of dictionaries containing information about the matches found.
         """
-        rg = Ripgrepy(self.pattern, str(self.directory)).i().with_filename().line_number().no_heading().glob(f'**/*.{self.extension}').json()
+        rg = (
+            Ripgrepy(self.pattern, str(self.directory))
+            .i()
+            .with_filename()
+            .line_number()
+            .no_heading()
+            .glob(f"**/*.{self.extension}")
+            .json()
+        )
         results = rg.run().as_dict
         return results
 
-    def write_matches_to_template(self, matches: List[Dict], template: Template, output_file: PathLike) -> bool:
+    def write_matches_to_template(
+        self, matches: List[Dict], template: Template, output_file: PathLike
+    ) -> bool:
         """
         Writes the matches found to the specified output file using the specified template.
 
@@ -55,32 +61,34 @@ class PatternAbstractor:
         - bool: True if the output was successfully written to the file, False otherwise.
         """
         try:
-            with open(output_file, 'w') as outfile:
+            with open(output_file, "w") as outfile:
                 bar = None
                 if self.debug:
-                    bar = Bar('Processing Matches', max=len(matches))
+                    bar = Bar("Processing Matches", max=len(matches))
                     bar.start()
 
                 outputs = []
                 for res in matches:
-                    match = res['data']
-                    file_path = match['path']['text']
-                    line_number = match['line_number']
-                    line_content = match['lines']['text']
+                    match = res["data"]
+                    file_path = match["path"]["text"]
+                    line_number = match["line_number"]
+                    line_content = match["lines"]["text"]
 
-                    for submatch in match['submatches']:
-                        text = submatch['match']['text']
-                        match_start_index = submatch['start']
-                        match_end_index = submatch['end']
+                    for submatch in match["submatches"]:
+                        text = submatch["match"]["text"]
+                        match_start_index = submatch["start"]
+                        match_end_index = submatch["end"]
 
-                        outputs.append({
-                            'file_path': file_path,
-                            'line_number': line_number,
-                            'line_content': line_content,
-                            'text': text,
-                            'match_start_index': match_start_index,
-                            'match_end_index': match_end_index
-                            })
+                        outputs.append(
+                            {
+                                "file_path": file_path,
+                                "line_number": line_number,
+                                "line_content": line_content,
+                                "text": text,
+                                "match_start_index": match_start_index,
+                                "match_end_index": match_end_index,
+                            }
+                        )
                     if bar:
                         bar.next()
 
@@ -90,6 +98,5 @@ class PatternAbstractor:
             return True
 
         except FileNotFoundError:
-            print(f'ERROR: Could not edit or create output_file {output_file}')
+            print(f"ERROR: Could not edit or create output_file {output_file}")
             return False
-
